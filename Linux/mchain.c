b091c898201a60443220de0a7aa4e44b7928a0a0
root <root@myhost.localdomain>
2011-04-16 07:10:14 -0500

    added initial checking for linux modification and test file

---
+++ Linux/mchain.c
@@ -0,0 +1,34 @@
+#include <linux/mm.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+#include <linux/linkage.h>
+
+SYSCALL_DEFINE0(new_mem_chain) {
+    return 0;
+}
+
+asmlinkage long sys_set_mem_chain_attr(unsigned int c,
+				       const memory_chain_attr_t * attr) {
+    return 0;
+}
+
+SYSCALL_DEFINE3(link_addr_rng, unsigned int, c, unsigned long, start,
+		size_t, length) {
+    return 0;
+}
+
+SYSCALL_DEFINE2(anchor, unsigned int, c, unsigned long, addr) {
+    return 0;
+}
+
+SYSCALL_DEFINE2(unlink_addr_rng, unsigned long, start, size_t, length) {
+    return 0;
+}
+
+SYSCALL_DEFINE1(brk_mem_chain, unsigned int, c) {
+    return 0;
+}
+
+SYSCALL_DEFINE1(rls_mem_chain, unsigned int, c) {
+    return 0;
+}
---
+++ Linux/test-syscalls.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/syscall.h>
+#include <linux/unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#define NR_sys_new_mem_chain      341
+#define NR_sys_set_mem_chain_attr 342
+#define NR_sys_link_addr_rng      343
+#define NR_sys_anchor             344
+#define NR_sys_unlink_addr_rng    345
+#define NR_sys_brk_mem_chain      346
+#define NR_sys_rls_mem_chain      347
+
+int main() {
+
+    int i = 0;
+
+    int passed = 0;
+    int nr_passed = 0;
+
+    
+    const char * sys_calls[] = {
+        "new_mem_chain(void)",
+	"set_mem_chain_attr(unsigned int, const memory_chain_attr *)",
+	"link_addr_rng(unsigned int, void *, size_t)",
+	"anchor(unsigned int, void *)",
+	"unlink_addr_rng(unsigned int, void *, size_t)",
+	"brk_mem_chain(unsigned int)",
+	"rls_mem_chain(unsigned int)"
+    };
+
+    int n = sizeof(sys_calls) / sizeof(sys_calls[0]);
+    int total = n;
+
+    fprintf(stdout, "testing system calls...\n");
+    for (i = 0; i < n; ) {
+
+        /* syscall does not perform any parameter checking/validation. */
+        passed = syscall(341+i) == 0;
+	 
+        fprintf(stdout, "test %d: %s...", ++i, sys_calls[i]);  
+	fprintf(stdout, "%s\n", strerror(errno));
+	nr_passed += passed;
+    }      
+    fprintf(stdout, "test complete. passed %d of %d\n",
+	    nr_passed, total);
+
+    return 0;
+}
