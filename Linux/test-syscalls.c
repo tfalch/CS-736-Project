9d273038bf81d7e3fcdc8e297284b22097c88eed
Greig Hazell <g_hazell@hotmail.com>
2011-04-23 12:41:06 -0500

    create test suite

--- Linux/test-syscalls.c
+++ Linux/test-syscalls.c
@@ -5,12 +5,98 @@
 
 #include "mchain.h"
 
+#define TEST_RESULT(r, e) r == e ? "passed" : mchain_strerror(r)
 
 #define NPAGES 2
 #define LENGTH sizeof(int) * 1024 * NPAGES
 
+void exec_test_suite();
+void test_create_rls_mchain();
+void test_link_dynamic();
+void test_link_static();
+
 int main() {
+  exec_test_suite();
+  return 0;
+}
+
+void exec_test_suite() {
+
+  void (*test_suite[])(void) = {
+    &test_create_rls_mchain,
+    &test_link_dynamic,
+    NULL
+  };
   
+  int f = 0;
+  for ( ; test_suite[f]; f++) {
+    (*test_suite[f])();
+  }
+}
+
+void test_link_dynamic() {
+
+  int test_nr = 1;
+  int chain = mchain();
+  int len = sizeof(int) * 4000;
+  int * array = malloc(len);
+  int r = 0;
+
+  fprintf(stdout, 
+	  "Test: Link Dynamically Allocated Memory\n"	\
+	  "=======================================\n");
+
+  r = mlink(chain, array, len);
+  fprintf(stdout, "Test %d: Link Pages On Heap. Result=%s\n", test_nr++,
+	  TEST_RESULT(r, 0));      
+
+  r = rls_mchain(chain);
+  fprintf(stdout, "Test %d: Release Memory Chain. Result=%s\n", test_nr++,
+	  TEST_RESULT(r, 0));
+
+  fprintf(stdout, "Test Complete\n");
+
+}
+
+void test_create_rls_mchain() {
+  
+  int i = 0;
+  int chains[5];
+  int max_chains = 5;
+  int test_nr = 1;
+
+  fprintf(stdout, 
+	  "Test: Create & Release Multiple Memory Chains\n" \
+	  "=============================================\n");
+
+  for (i = 0; i < max_chains; i++) {  
+    chains[i] = mchain();
+    fprintf(stdout, "Test %d: Create Memory Chain. Result=%s\n", test_nr++,
+	    TEST_RESULT(chains[i] >= 0, 1));      
+  }
+
+  i = mchain();
+  fprintf(stdout, "Test %d: Create Memory Chain. Result=%s\n", test_nr++,
+	  TEST_RESULT(i, -1));      
+
+  for (i = 0; i < max_chains; i++) {
+    int r = rls_mchain(chains[i]);
+    fprintf(stdout, "Test %d: Release Memory Chain. Result=%s\n", test_nr++,
+	    TEST_RESULT(r, 0));      
+  }
+
+  i = mchain();
+  fprintf(stdout, "Test %d: Create Memory Chain. Result=%s\n", test_nr++,
+	  TEST_RESULT(i, 0));
+
+  rls_mchain(i);
+	    
+  fprintf(stdout, "Test Complete\n");
+}
+
+/*
+main()
+ 
   int i = 0;
   int * array = malloc(LENGTH);
 
@@ -44,10 +130,6 @@ int main() {
       fprintf(stderr, "%d: %s\n", id, mchain_strerror(id));
   }
 
-  return 0;
-}
-
-
 int test_sys_calls() {
 
     int i = 0;
@@ -72,7 +154,7 @@ int test_sys_calls() {
     fprintf(stdout, "testing system calls...\n");
     for (i = 0; i < n; ) {
 
-        /* syscall does not perform any parameter checking/validation. */
+        /* syscall does not perform any parameter checking/validation. * /
         passed = syscall(341+i) == 0;
 	 
         fprintf(stdout, "test %d: %s...", ++i, sys_calls[i]);  
@@ -84,3 +166,4 @@ int test_sys_calls() {
 
 
 }
+*/
